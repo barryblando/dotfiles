d5b23a638f4327b554d18f31de25206a2ec5b93e
barryblando <barryblando@gmail.com>
2022-07-18 21:14:52 +0800

    add built-in diagnostics source link back

--- neovim/.config/nvim/lua/lsp/null-ls.lua
+++ neovim/.config/nvim/lua/lsp/null-ls.lua
@@ -5,6 +5,8 @@ end
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
 local formatting = null_ls.builtins.formatting
+-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup({
 	debug = false,
