ba8e48f28262b0794c2079a2c1771f13b47ce820
barryblando <barryblando@gmail.com>
2022-08-20 19:54:44 +0800

    make this sumneko_lua optional

--- neovim/.config/nvim/lua/lsp/settings/sumneko_lua.lua
+++ neovim/.config/nvim/lua/lsp/settings/sumneko_lua.lua
@@ -1,7 +1,43 @@
-local ok_status, luadev = pcall(require, "lua-dev")
-
-if not ok_status then
-	return
-end
-
-return luadev.setup({})
+return {
+	settings = {
+		Lua = {
+      type = {
+				-- weakUnionCheck = true,
+				-- weakNilCheck = true,
+				-- castNumberToInteger = true,
+			},
+			format = {
+				enable = false,
+			},
+			hint = {
+				enable = true,
+				arrayIndex = "Disable", -- "Enable", "Auto", "Disable"
+				await = true,
+				paramName = "Disable", -- "All", "Literal", "Disable"
+				paramType = false,
+				semicolon = "Disable", -- "All", "SameLine", "Disable"
+				setType = true,
+			},
+			-- spell = {"the"}
+			runtime = {
+				version = "LuaJIT",
+				special = {
+					reload = "require",
+				},
+			},
+			diagnostics = {
+				globals = { "vim" },
+			},
+			workspace = {
+				library = {
+					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+					[vim.fn.stdpath("config") .. "/lua"] = true,
+					-- [vim.fn.datapath "config" .. "/lua"] = true,
+				},
+			},
+			telemetry = {
+				enable = false,
+			},
+		},
+	},
+}
