7d94a57eb9f0bfd2c09ee47ad0d34f33ba2f9654
barryblando <barryblando@gmail.com>
2022-08-20 19:57:32 +0800

    update neo-tree

--- neovim/.config/nvim/lua/plugins/nvim-neo-tree.lua
+++ neovim/.config/nvim/lua/plugins/nvim-neo-tree.lua
@@ -59,16 +59,40 @@ neotree.setup({
 				config = { show_path = "absolute" },
 			},
 		},
-	},
-	filesystem = {
-		group_empty_dirs = true,
-		bind_to_cwd = false,
-		hijack_netrw_behavior = "open_default",
-		use_libuv_file_watcher = true,
-		filtered_items = {
-			hide_dotfiles = false,
-			never_show = { ".git" },
-		},
+  },
+  filesystem = {
+    group_empty_dirs = true,
+    bind_to_cwd = false,
+    hijack_netrw_behavior = "open_default",
+    use_libuv_file_watcher = true,
+    filtered_items = {
+      hide_dotfiles = false,
+      never_show = { ".git" },
+    },
+    components = {
+      harpoon_index = function(config, node, state)
+        local Marked = require("harpoon.mark")
+        local path = node:get_id()
+        local succuss, index = pcall(Marked.get_index_of, path)
+        if succuss and index and index > 0 then
+          return {
+            text = string.format("=>%d ", index), -- <-- Add your favorite harpoon like arrow here
+            highlight = config.highlight or "NeoTreeDirectoryIcon",
+          }
+        else
+          return {}
+        end
+      end
+    },
+    renderers = {
+      file = {
+        {"icon"},
+        {"name", use_git_status_colors = true},
+        {"harpoon_index"}, --> This is what actually adds the component in where you want it
+        {"diagnostics"},
+        {"git_status", highlight = "NeoTreeDimText"},
+      }
+    }
 	},
 	buffers = {
 		follow_current_file = true, -- This will find and focus the file in the active buffer every
