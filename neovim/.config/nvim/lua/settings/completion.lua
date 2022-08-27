c077f1b2a7816042d5dc34715311b92bb4a5ac59
barryblando <barryblando@gmail.com>
2022-08-27 22:23:47 +0800

    update

--- neovim/.config/nvim/lua/settings/completion.lua
+++ neovim/.config/nvim/lua/settings/completion.lua
@@ -8,11 +8,34 @@ if not snip_status_ok then
 	return
 end
 
+local buffer_fts = {
+	"markdown",
+	"toml",
+	"yaml",
+	"json",
+}
+
+local function contains(t, value)
+	for _, v in pairs(t) do
+		if v == value then
+			return true
+		end
+	end
+	return false
+end
+
+local compare = require("cmp.config.compare")
+
 require("luasnip/loaders/from_vscode").lazy_load()
 
+-- local check_backspace = function()
+-- 	local col = vim.fn.col(".") - 1
+-- 	return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
+-- end
+
 local check_backspace = function()
-	local col = vim.fn.col(".") - 1
-	return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
 local icons = require("utils.icons")
@@ -21,7 +44,21 @@ local kind_icons = icons.kind
 
 -- find more here: https://www.nerdfonts.com/cheat-sheet
 
+-- vim.api.nvim_set_hl(0, "CmpItemKindCopilot", { fg = "#6CC644" })
+vim.api.nvim_set_hl(0, "CmpItemKindTabnine", { fg = "#CA42F0" })
+-- vim.api.nvim_set_hl(0, "CmpItemKindEmoji", { fg = "#FDE030" })
+vim.api.nvim_set_hl(0, "CmpItemKindCrate", { fg = "#F64D00" })
+
+vim.g.cmp_active = true
+
 cmp.setup({
+	enabled = function()
+		local buftype = vim.api.nvim_buf_get_option(0, "buftype")
+		if buftype == "prompt" then
+			return false
+		end
+		return vim.g.cmp_active
+	end,
 	snippet = {
 		expand = function(args)
 			luasnip.lsp_expand(args.body) -- For `luasnip` users.
@@ -75,21 +112,78 @@ cmp.setup({
 		format = function(entry, vim_item)
 			-- Kind icons
 			vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+
+			if entry.source.name == "cmp_tabnine" then
+				vim_item.kind = icons.misc.Robot
+				vim_item.kind_hl_group = "CmpItemKindTabnine"
+			end
+
+			if entry.source.name == "crates" then
+				vim_item.kind = icons.misc.Package
+				vim_item.kind_hl_group = "CmpItemKindCrate"
+			end
+
 			-- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+
 			vim_item.menu = ({
-				nvim_lsp = "[LSP]",
-				luasnip = "[Snippet]",
-				buffer = "[Buffer]",
-				path = "[Path]",
+				nvim_lsp = "",
+				nvim_lua = "",
+				luasnip = "",
+				buffer = "",
+				path = "",
 			})[entry.source.name]
+
 			return vim_item
 		end,
 	},
 	sources = {
-		{ name = "nvim_lsp" },
-		{ name = "luasnip" },
-		{ name = "buffer" },
-		{ name = "path" },
+		{ name = "crates", group_index = 1 },
+		{
+			name = "nvim_lsp",
+			filter = function(entry, _)
+				local kind = require("cmp.types.lsp").CompletionItemKind[entry:get_kind()]
+				if kind == "Snippet" then
+					return true
+				end
+
+				if kind == "Text" then
+					return true
+				end
+			end,
+			group_index = 2,
+		},
+		{ name = "nvim_lua", group_index = 2 },
+		{ name = "luasnip", group_index = 2 },
+		{
+			name = "buffer",
+			group_index = 2,
+			filter = function(_, ctx)
+				if not contains(buffer_fts, ctx.prev_context.filetype) then
+					return true
+				end
+			end,
+		},
+		{ name = "cmp_tabnine", group_index = 2 },
+		{ name = "path", group_index = 2 },
+	},
+	sorting = {
+		priority_weight = 2,
+		comparators = {
+			-- require("copilot_cmp.comparators").prioritize,
+			-- require("copilot_cmp.comparators").score,
+			compare.offset,
+			compare.exact,
+			-- compare.scopes,
+			compare.score,
+			compare.recently_used,
+			compare.locality,
+			-- compare.kind,
+			compare.sort_text,
+			compare.length,
+			compare.order,
+			-- require("copilot_cmp.comparators").prioritize,
+			-- require("copilot_cmp.comparators").score,
+		},
 	},
 	confirm_opts = {
 		behavior = cmp.ConfirmBehavior.Replace,
@@ -107,7 +201,6 @@ cmp.setup({
 		},
 	},
 	experimental = {
-		ghost_text = false,
-		native_menu = false,
+		ghost_text = true,
 	},
 })
