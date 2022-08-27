f4355e827c61f5e28fb1e8b066444c9656700202
barryblando <barryblando@gmail.com>
2022-08-27 22:19:53 +0800

    update lualine to inclue language server

--- neovim/.config/nvim/lua/plugins/lualine.lua
+++ neovim/.config/nvim/lua/plugins/lualine.lua
@@ -10,6 +10,20 @@ local hide_in_width = function()
 	return vim.fn.winwidth(0) > 80
 end
 
+local hl_str = function(str, hl)
+  return "%#" .. hl .. "#" .. str .. "%*"
+end
+
+-- check if value in table
+local function contains(t, value)
+  for _, v in pairs(t) do
+    if v == value then
+      return true
+    end
+  end
+  return false
+end
+
 local diagnostics = {
 	"diagnostics",
 	sources = { "nvim_diagnostic" },
@@ -81,6 +95,94 @@ local branch = {
 	icon = "",
 }
 
+local lanuage_server = {
+  function()
+    local buf_ft = vim.bo.filetype
+    local ui_filetypes = {
+      "help",
+      "packer",
+      "neogitstatus",
+      "NvimTree",
+      "Trouble",
+      "lir",
+      "Outline",
+      "neo-tree",
+      "spectre_panel",
+      "toggleterm",
+      "DressingSelect",
+      "TelescopePrompt",
+      "lspinfo",
+      "lsp-installer",
+      "",
+    }
+
+    if contains(ui_filetypes, buf_ft) then
+      if M.language_servers == nil then
+        return ""
+      else
+        return M.language_servers
+      end
+    end
+
+    local clients = vim.lsp.buf_get_clients()
+    local client_names = {}
+    local copilot_active = false
+
+    -- add client
+    for _, client in pairs(clients) do
+      if client.name ~= "copilot" and client.name ~= "null-ls" then
+        table.insert(client_names, client.name)
+      end
+      if client.name == "copilot" then
+        copilot_active = true
+      end
+    end
+
+    -- add formatter
+    local s = require "null-ls.sources"
+    local available_sources = s.get_available(buf_ft)
+    local registered = {}
+    for _, source in ipairs(available_sources) do
+      for method in pairs(source.methods) do
+        registered[method] = registered[method] or {}
+        table.insert(registered[method], source.name)
+      end
+    end
+
+    local formatter = registered["NULL_LS_FORMATTING"]
+    local linter = registered["NULL_LS_DIAGNOSTICS"]
+    if formatter ~= nil then
+      vim.list_extend(client_names, formatter)
+    end
+    if linter ~= nil then
+      vim.list_extend(client_names, linter)
+    end
+
+    -- join client names with commas
+    local client_names_str = table.concat(client_names, ", ")
+
+    -- check client_names_str if empty
+    local language_servers = ""
+    local client_names_str_len = #client_names_str
+    if client_names_str_len ~= 0 then
+      language_servers = " " .. client_names_str .. " "
+    end
+    if copilot_active then
+      language_servers = language_servers .. "%#SLCopilot#" .. " " .. icons.git.Octoface .. "%*"
+    end
+
+    if client_names_str_len == 0 and not copilot_active then
+      return ""
+    else
+      M.language_servers = language_servers
+      return language_servers:gsub(", anonymous source", "")
+    end
+  end,
+  padding = 0,
+  cond = hide_in_width,
+  -- separator = "%#SLSeparator#" .. " │" .. "%*",
+}
+
 local location = {
 	"location",
 	padding = 0,
@@ -107,7 +209,7 @@ local progress = function()
 end
 
 local spaces = function()
-	return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
+	return "Spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
 end
 
 lualine.setup({
@@ -124,7 +226,7 @@ lualine.setup({
 			left = icons.ui.TriangleRight,
 			right = icons.ui.TriangleLeft,
 		},
-		disabled_filetypes = { "alpha", "dashboard", "Outline", "neo-tree" },
+		disabled_filetypes = { "alpha", "dashboard", "Outline", "neo-tree", "saga" },
 		always_divide_middle = true,
 		globalstatus = true,
 	},
@@ -133,7 +235,7 @@ lualine.setup({
 		lualine_b = { branch, diff },
 		lualine_c = {},
 		-- lualine_x = { "encoding", "fileformat", "filetype" },
-		lualine_x = { diagnostics, spaces, "encoding", filetype },
+		lualine_x = { diagnostics, lanuage_server, spaces, "encoding", filetype },
 		lualine_y = { location },
 		lualine_z = { progress },
 	},
