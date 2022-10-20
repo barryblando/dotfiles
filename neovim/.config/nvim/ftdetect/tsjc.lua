d7565c976352522a769a3c2cb713fb3cc8797683
barryblando <barryblando@gmail.com>
2022-10-20 12:27:50 +0800

    update: to jsonc

--- neovim/.config/nvim/ftdetect/tsjc.lua
+++
@@ -1,6 +0,0 @@
-vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
-  pattern = { "tsconfig.json", "tsconfig.*.json" },
-  callback = function()
-    vim.cmd "setlocal filetype=jsonc"
-  end,
-})
