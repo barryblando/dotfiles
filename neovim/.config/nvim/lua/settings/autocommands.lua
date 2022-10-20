4d2e84479cc2d1cafdf26546267f6171416b0e10
barryblando <barryblando@gmail.com>
2022-10-20 14:27:48 +0800

    add autocmd for disable diagnostics in env files

--- neovim/.config/nvim/lua/settings/autocommands.lua
+++ neovim/.config/nvim/lua/settings/autocommands.lua
@@ -107,3 +107,12 @@ vim.api.nvim_create_autocmd({ "CursorHold" }, {
 		end
 	end,
 })
+
+local group = vim.api.nvim_create_augroup("__env", {clear=true})
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = "*.env*",
+  group = group,
+  callback = function(args)
+    vim.diagnostic.disable(args.buf)
+  end
+})
