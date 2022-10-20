0f52b4aad72537e25a00d024bd25b3e2d53d415d
barryblando <barryblando@gmail.com>
2022-10-20 12:28:33 +0800

    separate make script executable for sh files only

---
+++ neovim/.config/nvim/ftplugin/sh.lua
@@ -0,0 +1,19 @@
+local status_ok, which_key = pcall(require, "which-key")
+if not status_ok then
+  return
+end
+
+local opts = {
+  mode = "n", -- NORMAL mode
+  prefix = "<leader>",
+  buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
+  silent = true, -- use `silent` when creating keymaps
+  noremap = true, -- use `noremap` when creating keymaps
+  nowait = true, -- use `nowait` when creating keymaps
+}
+
+local mappings = {
+	["x"] = { "<cmd>!chmod +x %<cr>", "Make Script Executable" },
+}
+
+which_key.register(mappings, opts)
