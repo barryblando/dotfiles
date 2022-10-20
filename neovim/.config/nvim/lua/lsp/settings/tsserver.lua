1e41c665f9aa713ac42d08506e1d28ce0ad115c2
barryblando <barryblando@gmail.com>
2022-10-20 12:28:48 +0800

    disable inlayhints for now

--- neovim/.config/nvim/lua/lsp/settings/tsserver.lua
+++ neovim/.config/nvim/lua/lsp/settings/tsserver.lua
@@ -4,17 +4,17 @@ return {
 	filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "typescript.tsx" },
 	-- tsserver will only resolve projects with package.json
 	root_dir = util.root_pattern("package.json"),
-	settings = {
-		typescript = {
-			inlayHints = {
-				includeInlayEnumMemberValueHints = true,
-				includeInlayFunctionLikeReturnTypeHints = true,
-				includeInlayFunctionParameterTypeHints = true,
-				includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all';
-				includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-				includeInlayPropertyDeclarationTypeHints = true,
-				includeInlayVariableTypeHints = true,
-			},
-		},
-	},
+	-- settings = {
+	-- 	typescript = {
+	-- 		inlayHints = {
+	-- 			includeInlayEnumMemberValueHints = true,
+	-- 			includeInlayFunctionLikeReturnTypeHints = true,
+	-- 			includeInlayFunctionParameterTypeHints = true,
+	-- 			includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all';
+	-- 			includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+	-- 			includeInlayPropertyDeclarationTypeHints = true,
+	-- 			includeInlayVariableTypeHints = true,
+	-- 		},
+	-- 	},
+	-- },
 }
