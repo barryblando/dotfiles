6b7e8168f62719dda1a6d8f5da47881314f9e60d
barryblando <barryblando@gmail.com>
2022-11-24 10:47:34 +0800

    add gitsigns codeaction

--- neovim/.config/nvim/lua/lsp/null-ls.lua
+++ neovim/.config/nvim/lua/lsp/null-ls.lua
@@ -7,6 +7,8 @@ end
 local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
+-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/code_actions
+local code_actions = null_ls.builtins.code_actions
 
 null_ls.setup({
 	debug = false,
@@ -65,6 +67,9 @@ null_ls.setup({
 		-- shell scripts
 		-- diagnostics.shellcheck,
 	},
+  
+  -- gitsigns integration
+  code_actions.gitsigns,
 })
 
 local unwrap = {
