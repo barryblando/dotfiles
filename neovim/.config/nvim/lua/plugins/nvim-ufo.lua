9caf7205e464c1e872c6eb77ffcdde8ca4819241
barryblando <barryblando@gmail.com>
2022-11-25 21:58:27 +0800

    add options

--- neovim/.config/nvim/lua/plugins/nvim-ufo.lua
+++ neovim/.config/nvim/lua/plugins/nvim-ufo.lua
@@ -47,6 +47,11 @@ ufo.setup {
 local bufnr = vim.api.nvim_get_current_buf()
 ufo.setFoldVirtTextHandler(bufnr, handler)
 
+vim.o.foldcolumn = '0' -- '0' is not bad
+vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevelstart = 99
+
 -- keymaps
 vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
 vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
