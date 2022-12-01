f939dbdaabed4a5829e1f9d94419a10037214b54
barryblando <barryblando@gmail.com>
2022-12-01 15:12:56 +0800

    neovide updates

--- neovim/.config/nvim/lua/settings/colorscheme.lua
+++ neovim/.config/nvim/lua/settings/colorscheme.lua
@@ -42,10 +42,10 @@ vim.cmd([[
 -- Neovide config
 vim.cmd([[
   let g:neovide_scale_factor = 1.0
-  let g:neovide_transparency=0.8
+  let g:neovide_transparency=0.9
 
-  let g:neovide_floating_blur_amount_x = 2.0
-  let g:neovide_floating_blur_amount_y = 2.0
+  " let g:neovide_floating_blur_amount_x = 2.0
+  " let g:neovide_floating_blur_amount_y = 2.0
 
   let g:neovide_cursor_vfx_mode = "railgun"
   set guifont=MonoLisa,Jetbrains\ Mono:h13:#e-subpixelantialias
