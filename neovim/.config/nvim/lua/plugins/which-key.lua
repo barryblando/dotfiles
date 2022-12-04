0882948c65412540b8ccc8ab988402437ac466ba
barryblando <barryblando@gmail.com>
2022-12-04 23:14:14 +0800

    change to utils

--- neovim/.config/nvim/lua/plugins/which-key.lua
+++ neovim/.config/nvim/lua/plugins/which-key.lua
@@ -119,12 +119,10 @@ local mappings = {
     name = "Options",
     c = { '<cmd>lua vim.g.cmp_active=false<cr>', "Completion off" },
     C = { '<cmd>lua vim.g.cmp_active=true<cr>', "Completion on" },
-    w = { '<cmd>lua require("user.functions").toggle_option("wrap")<cr>', "Wrap" },
-    r = { '<cmd>lua require("user.functions").toggle_option("relativenumber")<cr>', "Relative" },
-    l = { '<cmd>lua require("user.functions").toggle_option("cursorline")<cr>', "Cursorline" },
-    s = { '<cmd>lua require("user.functions").toggle_option("spell")<cr>', "Spell" },
-    t = { '<cmd>lua require("user.functions").toggle_tabline()<cr>', "Tabline" },
-
+    w = { '<cmd>lua require("utils.functions").toggle_option("wrap")<cr>', "Wrap" },
+    r = { '<cmd>lua require("utils.functions").toggle_option("relativenumber")<cr>', "Relative" },
+    l = { '<cmd>lua require("utils.functions").toggle_option("cursorline")<cr>', "Cursorline" },
+    s = { '<cmd>lua require("utils.functions").toggle_option("spell")<cr>', "Spell" },
   },
 
 	s = {
