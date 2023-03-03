6ef72a1d77d68b4723262db5ffa8adcc1c7d9e2d
barryblando <barryblando@gmail.com>
2023-03-03 17:40:50 +0800

    update root dir

--- neovim/.config/nvim/lua/lsp/settings/tsserver.lua
+++ neovim/.config/nvim/lua/lsp/settings/tsserver.lua
@@ -3,7 +3,7 @@ local util = require("lspconfig").util
 return {
 	filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "typescript.tsx" },
 	-- tsserver will only resolve projects with package.json
-	root_dir = util.root_pattern("package.json"),
+	root_dir = util.root_pattern("package.json", ".git"),
 	-- settings = {
 	-- 	typescript = {
 	-- 		inlayHints = {
