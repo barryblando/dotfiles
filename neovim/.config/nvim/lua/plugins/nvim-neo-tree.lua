7d7f8449c5690a2a12e81f32bc904b64e6e8438b
barryblando <barryblando@gmail.com>
2023-03-04 07:40:11 +0800

    remove tg tf

--- neovim/.config/nvim/lua/plugins/nvim-neo-tree.lua
+++ neovim/.config/nvim/lua/plugins/nvim-neo-tree.lua
@@ -14,27 +14,27 @@ local function openWithFocus(state)
 	end
 end
 
-local function getTelescopeOpts(state, path)
-  return {
-    cwd = path,
-    search_dirs = { path },
-    attach_mappings = function (prompt_bufnr, map)
-      local actions = require "telescope.actions"
-      actions.select_default:replace(function()
-        actions.close(prompt_bufnr)
-        local action_state = require "telescope.actions.state"
-        local selection = action_state.get_selected_entry()
-        local filename = selection.filename
-        if (filename == nil) then
-          filename = selection[1]
-        end
-        -- any way to open the file without triggering auto-close event of neo-tree?
-        require("neo-tree.sources.filesystem").navigate(state, state.path, filename)
-      end)
-      return true
-    end
-  }
-end
+-- local function getTelescopeOpts(state, path)
+--   return {
+--     cwd = path,
+--     search_dirs = { path },
+--     attach_mappings = function (prompt_bufnr, map)
+--       local actions = require "telescope.actions"
+--       actions.select_default:replace(function()
+--         actions.close(prompt_bufnr)
+--         local action_state = require "telescope.actions.state"
+--         local selection = action_state.get_selected_entry()
+--         local filename = selection.filename
+--         if (filename == nil) then
+--           filename = selection[1]
+--         end
+--         -- any way to open the file without triggering auto-close event of neo-tree?
+--         require("neo-tree.sources.filesystem").navigate(state, state.path, filename)
+--       end)
+--       return true
+--     end
+--   }
+-- end
 
 neotree.setup({
 	close_if_last_window = false,
@@ -85,8 +85,26 @@ neotree.setup({
 				"add",
 				config = { show_path = "absolute" },
 			},
-      ["tf"] = "telescope_find",
-      ["tg"] = "telescope_grep",
+      -- ["tf"] = "telescope_find",
+      -- ["tg"] = "telescope_grep",
+      ["h"] = function(state)
+        local node = state.tree:get_node()
+        if node.type == 'directory' and node:is_expanded() then
+          require'neo-tree.sources.filesystem'.toggle_directory(state, node)
+        else
+          require'neo-tree.ui.renderer'.focus_node(state, node:get_parent_id())
+        end
+      end,
+      ["l"] = function(state)
+        local node = state.tree:get_node()
+        if node.type == 'directory' then
+          if not node:is_expanded() then
+            require'neo-tree.sources.filesystem'.toggle_directory(state, node)
+          elseif node:has_children() then
+            require'neo-tree.ui.renderer'.focus_node(state, node:get_child_ids()[1])
+          end
+        end
+      end,
 		},
   },
   filesystem = {
@@ -123,16 +141,16 @@ neotree.setup({
       }
     },
     commands = {
-      telescope_find = function(state)
-        local node = state.tree:get_node()
-        local path = node:get_id()
-        require('telescope.builtin').find_files(getTelescopeOpts(state, path))
-      end,
-      telescope_grep = function(state)
-        local node = state.tree:get_node()
-        local path = node:get_id()
-        require('telescope.builtin').live_grep(getTelescopeOpts(state, path))
-      end,
+      -- telescope_find = function(state)
+      --   local node = state.tree:get_node()
+      --   local path = node:get_id()
+      --   require('telescope.builtin').find_files(getTelescopeOpts(state, path))
+      -- end,
+      -- telescope_grep = function(state)
+      --   local node = state.tree:get_node()
+      --   local path = node:get_id()
+      --   require('telescope.builtin').live_grep(getTelescopeOpts(state, path))
+      -- end,
     },
 	},
 	buffers = {
