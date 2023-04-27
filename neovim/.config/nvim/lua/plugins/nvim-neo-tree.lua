df29a3167edda1e46839d4890e45e1680fef701a
barryblando <barryblando@gmail.com>
2023-04-27 21:43:25 +0800

    use release/2.0 branch

--- neovim/.config/nvim/lua/plugins/nvim-neo-tree.lua
+++ neovim/.config/nvim/lua/plugins/nvim-neo-tree.lua
@@ -8,7 +8,7 @@ return {
     {
       -- only needed if you want to use the commands with "_with_window_picker" suffix
       "s1n7ax/nvim-window-picker",
-      branch = "v2.*",
+      branch = "release/2.0",
       config = function()
         require("window-picker").setup({
           filter_rules = {
