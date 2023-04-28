8c1ce108cc2458e9ab2e2cba7ffb7523ab716718
barryblando <barryblando@gmail.com>
2023-04-28 09:45:29 +0800

    format

--- neovim/.config/nvim/lua/plugins/auto-session.lua
+++ neovim/.config/nvim/lua/plugins/auto-session.lua
@@ -1,4 +1,4 @@
-return { 
+return {
   "rmagatti/auto-session",
   config = function ()
     local status_ok, auto_session = pcall(require, "auto-session")
@@ -10,7 +10,7 @@ return {
     if not l_status_ok then
       return
     end
-    
+
     local opts = {
       log_level = "info",
       auto_session_enable_last_session = false,
