778bdd3dc2959987dc739e8cc3b0bb5566f0a606
barryblando <barryblando@gmail.com>
2023-06-15 11:00:50 +0800

    update schemas

--- neovim/.config/nvim/lua/lsp/settings/yamlls.lua
+++ neovim/.config/nvim/lua/lsp/settings/yamlls.lua
@@ -3,29 +3,29 @@ return {
 		yaml = {
 			-- other settings. note this overrides the lspconfig defaults.
 			schemas = {
-				["https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/v1.25.4-standalone-strict/all.json"] = {
+				["https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/v1.27.2-standalone-strict/all.json"] = {
 					"/infra/k8s/*.yml",
 					"/*.k8s.yml",
 				},
-				["https://raw.githubusercontent.com/GoogleContainerTools/skaffold/main/docs-v2/content/en/schemas/v4beta1.json"] = "skaffold.yml",
+				["https://raw.githubusercontent.com/GoogleContainerTools/skaffold/main/docs-v2/content/en/schemas/v4beta6.json"] = "skaffold.yml",
 				-- other schemas
 			},
 		},
 	},
 }
 
-  -- settings = {
-  --   yaml = {
-  --     trace = {
-  --       server = "verbose"
-  --     },
-  --     schemas = {
-  --       ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
-  --       ["http://json.schemastore.org/kustomization"] = "kustomization.yaml",
-  --       ["https://json.schemastore.org/chart.json"] = "Chart.yaml",
-  --       ["https://json.schemastore.org/taskfile.json"] = "Taskfile*.yml",
-  --       ["https://raw.githubusercontent.com/GoogleContainerTools/skaffold/master/docs/content/en/schemas/v2beta26.json"] = "skaffold.yaml",
-  --       ["https://raw.githubusercontent.com/rancher/k3d/main/pkg/config/v1alpha3/schema.json"] = "k3d.yaml",
-  --       ["https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/v1.20.13/all.json"] = "/*.yaml",
-  --     }
-  --   }
+-- settings = {
+--   yaml = {
+--     trace = {
+--       server = "verbose"
+--     },
+--     schemas = {
+--       ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
+--       ["http://json.schemastore.org/kustomization"] = "kustomization.yaml",
+--       ["https://json.schemastore.org/chart.json"] = "Chart.yaml",
+--       ["https://json.schemastore.org/taskfile.json"] = "Taskfile*.yml",
+--       ["https://raw.githubusercontent.com/GoogleContainerTools/skaffold/master/docs/content/en/schemas/v2beta26.json"] = "skaffold.yaml",
+--       ["https://raw.githubusercontent.com/rancher/k3d/main/pkg/config/v1alpha3/schema.json"] = "k3d.yaml",
+--       ["https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/v1.20.13/all.json"] = "/*.yaml",
+--     }
+--   }
