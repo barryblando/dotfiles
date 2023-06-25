f1be0d8e75ab8659a0125ce8b04920d1913e399c
barryblando <barryblando@gmail.com>
2023-06-25 21:50:46 +0800

    refactor

--- neovim/.config/nvim/lua/plugins/ui.lua
+++ neovim/.config/nvim/lua/plugins/ui.lua
@@ -15,106 +15,167 @@ return {
 				return vim.ui.input(...)
 			end
 		end,
-		opts = {
-			input = {
-				border = { "┌", "─", "┐", "│", "┘", "─", "└", "│" },
-				win_options = {
-					winblend = 0,
-				},
-			},
-			select = {
-				telescope = require("telescope.themes").get_dropdown({ -- or 'cursor'
-					borderchars = {
-						{ "─", "│", "─", "│", "┌", "┐", "┘", "└" },
-						prompt = { "─", "│", " ", "│", "┌", "┐", "│", "│" },
-						results = { "─", "│", "─", "│", "├", "┤", "┘", "└" },
-						preview = { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
-					},
-					-- layout_config = {
-					--   height = function(self, _, max_lines)
-					--     return random_height_i_computed
-					--   end,
-					-- },
-				}),
-				builtin = {
+		config = function()
+			local icons = require("utils.icons")
+
+			require("dressing").setup({
+				input = {
+					border = icons.ui.Border_Chars_Single_Line,
 					win_options = {
-						winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
+						winblend = 0,
 					},
 				},
-			},
-		},
+				select = {
+					telescope = require("telescope.themes").get_dropdown({ -- or 'cursor'
+						borderchars = icons.ui.Border_Chars,
+						-- layout_config = {
+						--   height = function(self, _, max_lines)
+						--     return random_height_i_computed
+						--   end,
+						-- },
+					}),
+					builtin = {
+						border = icons.ui.Border_Chars_Single_Line,
+						win_options = {
+							winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
+						},
+					},
+				},
+			})
+		end,
 	},
 
 	-- noicer ui
 	{
 		"folke/noice.nvim",
 		event = "VeryLazy",
-		opts = {
-			views = {
-				cmdline_popup = {
-					border = {
-						style = { "┏", "━", "┓", "┃", "┛", "━", "┗", "┃" },
-						-- padding = { 1, 1 },
+		config = function()
+			local icons = require("utils.icons")
+
+			require("noice").setup({
+				views = {
+					cmdline_popup = {
+						border = {
+							style = icons.ui.Border_Single_Line,
+							-- padding = { 1, 1 },
+						},
+						filter_options = {},
+						win_options = {
+							winhighlight = {
+								Normal = "NormalFloat",
+								FloatBorder = "FloatBorder",
+							},
+						},
 					},
-					filter_options = {},
-					win_options = {
-						winhighlight = {
-							Normal = "NormalFloat",
-							FloatBorder = "FloatBorder",
+					cmdline_popupmenu = {
+						view = "popupmenu",
+						zindex = 200,
+						border = {
+							style = icons.ui.Border_Single_Line,
+							-- padding = { 1, 1 },
+						},
+					},
+					popupmenu = {
+						win_options = {
+							winhighlight = { Normal = "Normal", FloatBorder = "FloatBorder" },
+						},
+					},
+					popup = {
+						border = {
+							style = icons.ui.Border_Single_Line,
+						},
+						win_options = {
+							winhighlight = {
+								Normal = "NormalFloat",
+								FloatBorder = "FloatBorder",
+							},
 						},
 					},
 				},
-				popupmenu = {
-					border = {
-						style = { "┏", "━", "┓", "┃", "┛", "━", "┗", "┃" },
+				lsp = {
+					override = {
+						["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+						["vim.lsp.util.stylize_markdown"] = true,
 					},
-					win_options = {
-						winhighlight = {},
+					signature = {
+						enabled = false,
 					},
-				},
-				popup = {
-					border = {
-						style = { "┏", "━", "┓", "┃", "┛", "━", "┗", "┃" },
+					progress = {
+						enabled = false,
 					},
-					win_options = {
-						winhighlight = {
-							Normal = "NormalFloat",
-							FloatBorder = "FloatBorder",
-						},
+					hover = {
+						enabled = false,
 					},
 				},
+				presets = {
+					bottom_search = false,
+					command_palette = true,
+					long_message_to_split = true,
+					inc_rename = true,
+				},
+			})
+		end,
+		keys = {
+			{
+				"<S-Enter>",
+				function()
+					require("noice").redirect(vim.fn.getcmdline())
+				end,
+				mode = "c",
+				desc = "Redirect Cmdline",
 			},
-			lsp = {
-				override = {
-					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-					["vim.lsp.util.stylize_markdown"] = true,
-				},
-				signature = {
-					enabled = false,
-				},
-				progress = {
-					enabled = false,
-				},
-				hover = {
-					enabled = false,
-				},
+			{
+				"<leader>snl",
+				function()
+					require("noice").cmd("last")
+				end,
+				desc = "Noice Last Message",
 			},
-			presets = {
-				bottom_search = false,
-				command_palette = true,
-				long_message_to_split = true,
-				inc_rename = true,
+			{
+				"<leader>snh",
+				function()
+					require("noice").cmd("history")
+				end,
+				desc = "Noice History",
+			},
+			{
+				"<leader>sna",
+				function()
+					require("noice").cmd("all")
+				end,
+				desc = "Noice All",
+			},
+			{
+				"<leader>snd",
+				function()
+					require("noice").cmd("dismiss")
+				end,
+				desc = "Dismiss All",
+			},
+			{
+				"<c-f>",
+				function()
+					if not require("noice.lsp").scroll(4) then
+						return "<c-f>"
+					end
+				end,
+				silent = true,
+				expr = true,
+				desc = "Scroll forward",
+				mode = { "i", "n", "s" },
+			},
+			{
+				"<c-b>",
+				function()
+					if not require("noice.lsp").scroll(-4) then
+						return "<c-b>"
+					end
+				end,
+				silent = true,
+				expr = true,
+				desc = "Scroll backward",
+				mode = { "i", "n", "s" },
 			},
 		},
-    -- stylua: ignore
-    keys = {
-      { "<S-Enter>", function() require("noice").redirect(vim.fn.getcmdline()) end, mode = "c", desc = "Redirect Cmdline" },
-      { "<leader>snl", function() require("noice").cmd("last") end, desc = "Noice Last Message" },
-      { "<leader>snh", function() require("noice").cmd("history") end, desc = "Noice History" },
-      { "<leader>sna", function() require("noice").cmd("all") end, desc = "Noice All" },
-      { "<leader>snd", function() require("noice").cmd("dismiss") end, desc = "Dismiss All" },
-      { "<c-f>", function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end, silent = true, expr = true, desc = "Scroll forward", mode = {"i", "n", "s"} },
-      { "<c-b>", function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true, expr = true, desc = "Scroll backward", mode = {"i", "n", "s"}},
-    },
 	},
 }
