8f9a6f98a296466a28f6f0bdcafb4191ea2d92f0
barryblando <barryblando@gmail.com>
2023-07-10 11:36:04 +0800

    update config

--- neovim/.config/nvim/lua/plugins/nvim-lightbulb.lua
+++ neovim/.config/nvim/lua/plugins/nvim-lightbulb.lua
@@ -1,42 +1,59 @@
 return {
-  "kosayoda/nvim-lightbulb",
-  config = function ()
-    local ok_status, lightbulb = pcall(require, "nvim-lightbulb")
-
-    if not ok_status then
-      return
-    end
-
-    lightbulb.setup({
-      ignore = { "null-ls" },
-      sign = {
-        enabled = false,
-      },
-      virtual_text = {
-        enabled = true,
-        text = "💡",
-        hl_mode = "combine",
-      },
-      status_text = {
-        enabled = true,
-        text = " Code Action Available",
-        -- text = " Code Action Available",
-        text_unavailable = "",
-      },
-    })
-
-    local function update()
-      -- update status
-      lightbulb.update_lightbulb()
-
-      -- display status
-      local status = lightbulb.get_status_text()
-      vim.api.nvim_echo({ { status, "WarningMsg" } }, false, {})
-    end
-
-    vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-      desc = "Check for available code actions",
-      callback = update,
-    })
-  end
+	"kosayoda/nvim-lightbulb",
+	opts = {
+		ignore = {
+			clients = { "null-ls" },
+			ft = { "neo-tree" },
+		},
+		sign = {
+			enabled = true,
+			text = "",
+			-- Highlight group to highlight the sign column text.
+			hl = "LightBulbSign",
+		},
+		virtual_text = {
+			enabled = false,
+			-- Text to show in the virt_text.
+			text = "",
+			-- Position of virtual text given to |nvim_buf_set_extmark|.
+			-- Can be a number representing a fixed column (see `virt_text_pos`).
+			-- Can be a string representing a position (see `virt_text_win_col`).
+			pos = "eol",
+			-- Highlight group to highlight the virtual text.
+			hl = "LightBulbVirtualText",
+			-- How to combine other highlights with text highlight.
+			-- See `hl_mode` of |nvim_buf_set_extmark|.
+			hl_mode = "combine",
+		},
+		float = {
+			enabled = false,
+			-- Text to show in the floating window.
+			text = "💡",
+			-- Highlight group to highlight the floating window.
+			hl = "LightBulbFloatWin",
+			-- Window options.
+			-- See |vim.lsp.util.open_floating_preview| and |nvim_open_win|.
+			-- Note that some options may be overridden by |open_floating_preview|.
+			win_opts = {
+				focusable = false,
+			},
+		},
+		status_text = {
+			enabled = false,
+			text = " Code Action Available",
+			-- text = " Code Action Available",
+			text_unavailable = "",
+		},
+		autocmd = {
+			-- Whether or not to enable autocmd creation.
+			enabled = true,
+			-- See |updatetime|.
+			-- Set to a negative value to avoid setting the updatetime.
+			updatetime = 200,
+			-- See |nvim_create_autocmd|.
+			events = { "CursorHold", "CursorHoldI" },
+			-- See |nvim_create_autocmd| and |autocmd-pattern|.
+			pattern = { "*" },
+		},
+	},
 }
