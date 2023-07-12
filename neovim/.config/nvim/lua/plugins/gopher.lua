9b00777dcf6811aa90e7e9e180dfc95e7d1735ef
barryblando <barryblando@gmail.com>
2023-07-12 18:15:03 +0800

    initial commit

---
+++ neovim/.config/nvim/lua/plugins/gopher.lua
@@ -0,0 +1,11 @@
+return {
+	"olexsmir/gopher.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-treesitter/nvim-treesitter",
+	},
+	ft = "go",
+	build = function()
+		vim.cmd([[silent! GoInstallDeps]])
+	end,
+}
