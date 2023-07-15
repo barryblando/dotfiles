3d27095f871a7331e4d7eae2cdedd4ffbd53ac27
barryblando <barryblando@gmail.com>
2023-07-15 13:36:12 +0800

    update config

--- neovim/.config/nvim/lua/plugins/typescript-tools.lua
+++ neovim/.config/nvim/lua/plugins/typescript-tools.lua
@@ -2,5 +2,18 @@ return {
 	"pmizio/typescript-tools.nvim",
 	enabled = true,
 	dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-	opts = {},
+	opts = {
+		setttings = {
+			separate_diagnostic_server = true,
+			publish_diagnostic_on = "change",
+			tsserver_file_preferences = {
+				includeInlayParameterNameHints = "all",
+				includeInlayEnumMemberValueHints = true,
+				includeInlayFunctionLikeReturnTypeHints = true,
+				includeInlayFunctionParameterTypeHints = true,
+				includeInlayPropertyDeclarationTypeHints = true,
+				includeInlayVariableTypeHints = true,
+			},
+		},
+	},
 }
