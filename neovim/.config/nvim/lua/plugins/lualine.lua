6463d06a27552c569b4548b4c0878bfe9af34280
barryblando <barryblando@gmail.com>
2023-07-27 20:31:07 +0800

    update theme

--- neovim/.config/nvim/lua/plugins/lualine.lua
+++ neovim/.config/nvim/lua/plugins/lualine.lua
@@ -290,11 +290,20 @@ return {
 			color = { fg = "#ff9e64" },
 		}
 
+		local auto_theme_custom = require("lualine.themes.auto")
+
+		auto_theme_custom.normal.c.bg = "none"
+		auto_theme_custom.replace.c.bg = "none"
+		auto_theme_custom.command.c.bg = "none"
+		auto_theme_custom.insert.c.bg = "none"
+		auto_theme_custom.visual.c.bg = "none"
+		auto_theme_custom.terminal.c.bg = "none"
+
 		lualine.setup({
 			options = {
 				icons_enabled = true,
 				-- theme = require("settings.lualineTheme").theme(),
-				theme = "auto",
+				theme = auto_theme_custom,
 				-- component_separators = { left = "", right = "" },
 				component_separators = {
 					left = "",
