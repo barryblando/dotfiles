9f176611c9901eab85f1ac581ebbb0877b6bf07a
barryblando <barryblando@gmail.com>
2023-07-29 20:14:49 +0800

    update window picker

--- neovim/.config/nvim/lua/plugins/nvim-neo-tree.lua
+++ neovim/.config/nvim/lua/plugins/nvim-neo-tree.lua
@@ -8,7 +8,9 @@ return {
 		{
 			-- only needed if you want to use the commands with "_with_window_picker" suffix
 			"s1n7ax/nvim-window-picker",
-			branch = "release/2.0",
+			name = "window-picker",
+			event = "VeryLazy",
+			version = "2.*",
 			config = function()
 				require("window-picker").setup({
 					filter_rules = {
