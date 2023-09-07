d92ca359ffee5f9341075e1a3ebf709f865e7547
barryblando <barryblando@gmail.com>
2023-09-07 09:41:48 +0800

    cleanup

--- neovim/.config/nvim/lua/plugins/init.lua
+++ neovim/.config/nvim/lua/plugins/init.lua
@@ -1,13 +1,4 @@
--- local function tabnine_build_path()
--- 	if vim.loop.os_uname().sysname == "Windows_NT" then
--- 		return "pwsh.exe -file .\\dl_binaries.ps1"
--- 	else
--- 		return "./dl_binaries.sh"
--- 	end
--- end
-
 return {
-
 	---------------------
 	--  COLOR SCHEMES  --
 	---------------------
@@ -119,45 +110,6 @@ return {
 			"rafamadriz/friendly-snippets", -- a bunch of snippets to use
 		},
 	},
-
-	--  {
-	--    "codota/tabnine-nvim",
-	--    build = tabnine_build_path(),
-	--    opts = {
-	--      disable_auto_comment=true,
-	--      accept_keymap="<Tab>",
-	--      dismiss_keymap = "<C-]>",
-	--      debounce_ms = 800,
-	--      suggestion_color = {gui = "#808080", cterm = 244},
-	--      exclude_filetypes = {"TelescopePrompt"},
-	--      log_file_path = nil, -- absolute path to Tabnine log file
-	--    }
-	--  },
-
-	-- {
-	-- 	"tzachar/cmp-tabnine",
-	-- 	build = "./install.sh",
-	-- 	dependencies = "hrsh7th/nvim-cmp",
-	-- 	config = function()
-	-- 		local status_ok, tabnine = pcall(require, "cmp_tabnine.config")
-
-	-- 		if not status_ok then
-	-- 			return
-	-- 		end
-
-	-- 		tabnine:setup({
-	-- 			max_lines = 1000,
-	-- 			max_num_results = 20,
-	-- 			sort = true,
-	-- 			run_on_every_keystroke = true,
-	-- 			snippet_placeholder = "..",
-	-- 			ignored_file_types = { -- default is not to ignore
-	-- 				-- uncomment to ignore in lua:
-	-- 				-- lua = true
-	-- 			},
-	-- 		})
-	-- 	end,
-	-- },
 
 	---------------------
 	--      LSP        --
