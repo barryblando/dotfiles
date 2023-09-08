2e730f129fd7c0c057d6dd7ce5cf55ba442a99b9
barryblando <barryblando@gmail.com>
2023-09-08 08:34:08 +0800

    enable back neoscroll

--- neovim/.config/nvim/lua/plugins/neoscroll.lua
+++ neovim/.config/nvim/lua/plugins/neoscroll.lua
@@ -1,27 +1,33 @@
 return {
-  "karb94/neoscroll.nvim",
-  enabled = false,
-  config = function ()
-    local ok_satus, neoscroll = pcall(require, "neoscroll")
+	"karb94/neoscroll.nvim",
+	config = function()
+		local ok_satus, neoscroll = pcall(require, "neoscroll")
 
-    if not ok_satus then
-      return
-    end
-    
-    neoscroll.setup({
-      easing_function = nil,
-    })
+		if not ok_satus then
+			return
+		end
 
-    local t = {}
+		neoscroll.setup({
+			hide_cursor = true, -- Hide cursor while scrolling
+			stop_eof = true, -- Stop at <EOF> when scrolling downwards
+			respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+			cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
+			easing_function = nil, -- Default easing function
+			pre_hook = nil, -- Function to run before the scrolling animation starts
+			post_hook = nil, -- Function to run after the scrolling animation ends
+			performance_mode = true, -- Disable "Performance Mode" on all buffers.
+		})
 
-    -- Syntax: t[keys] = {function, {function arguments}}
-    -- Use the "sine" easing function
-    t["<C-u>"] = { "scroll", { "-vim.wo.scroll", "true", "350", [['sine']] } }
-    t["<C-d>"] = { "scroll", { "vim.wo.scroll", "true", "350", [['sine']] } }
-    -- Use the "circular" easing function
-    t["<C-b>"] = { "scroll", { "-vim.api.nvim_win_get_height(0)", "true", "500", [['circular']] } }
-    t["<C-f>"] = { "scroll", { "vim.api.nvim_win_get_height(0)", "true", "500", [['circular']] } }
+		local t = {}
 
-    require("neoscroll.config").set_mappings(t)
-  end
+		-- Syntax: t[keys] = {function, {function arguments}}
+		-- Use the "sine" easing function
+		t["<C-u>"] = { "scroll", { "-vim.wo.scroll", "true", "350", [['sine']] } }
+		t["<C-d>"] = { "scroll", { "vim.wo.scroll", "true", "350", [['sine']] } }
+		-- Use the "circular" easing function
+		t["<C-b>"] = { "scroll", { "-vim.api.nvim_win_get_height(0)", "true", "500", [['circular']] } }
+		t["<C-f>"] = { "scroll", { "vim.api.nvim_win_get_height(0)", "true", "500", [['circular']] } }
+
+		require("neoscroll.config").set_mappings(t)
+	end,
 }
