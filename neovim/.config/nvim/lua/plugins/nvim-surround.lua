079772b2943ba531a04a33e16bf15b365e81a7a9
barryblando <barryblando@gmail.com>
2023-09-10 13:17:32 +0800

    update

--- neovim/.config/nvim/lua/plugins/nvim-surround.lua
+++ neovim/.config/nvim/lua/plugins/nvim-surround.lua
@@ -1,24 +1,27 @@
 return {
-  "kylechui/nvim-surround",
-  config = function ()
-    local status_ok, surround = pcall(require, "nvim-surround")
-    if not status_ok then
-      return
-    end
+	"kylechui/nvim-surround",
+	-- enabled = false,
+	version = "*", -- Use for stability; omit to use `main` branch for the latest features
+	event = "VeryLazy",
+	config = function()
+		local status_ok, surround = pcall(require, "nvim-surround")
+		if not status_ok then
+			return
+		end
 
-    -- https://vimhelp.org/motion.txt.html#object-select
-    surround.setup({
-      keymaps = {
-        insert = "ys",
-        insert_line = "yss",
-        visual = "S",
-        delete = "ds",
-        change = "cs",
-      },
-      -- surrounds = {}, -- Gotta use the default config, https://github.com/kylechui/nvim-surround/blob/main/lua/nvim-surround/config.lua
-      highlight = { -- Highlight before inserting/changing surrounds
-        duration = 0,
-      },
-    })
-  end
+		-- https://vimhelp.org/motion.txt.html#object-select
+		surround.setup({
+			keymaps = {
+				insert = "ys",
+				insert_line = "yss",
+				visual = "S",
+				delete = "ds",
+				change = "cs",
+			},
+			-- surrounds = {}, -- Gotta use the default config, https://github.com/kylechui/nvim-surround/blob/main/lua/nvim-surround/config.lua
+			highlight = { -- Highlight before inserting/changing surrounds
+				duration = 0,
+			},
+		})
+	end,
 }
