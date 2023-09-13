3c8ada9f28ef16cd73fea28d831e21a281437e28
barryblando <barryblando@gmail.com>
2023-09-13 09:03:09 +0800

    enable exposed as code action

--- neovim/.config/nvim/lua/plugins/typescript-tools.lua
+++ neovim/.config/nvim/lua/plugins/typescript-tools.lua
@@ -6,6 +6,7 @@ return {
 		setttings = {
 			separate_diagnostic_server = true,
 			publish_diagnostic_on = "change",
+      expose_as_code_action = "all",
 			tsserver_file_preferences = {
 				includeInlayParameterNameHints = "all",
 				includeInlayEnumMemberValueHints = true,
