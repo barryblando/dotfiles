6f8cc974eea61b4721175d7030fecc3d03eedca0
barryblando <barryblando@gmail.com>
2023-09-28 18:55:08 +0800

    replace guard with conform

--- neovim/.config/nvim/lua/plugins/init.lua
+++ neovim/.config/nvim/lua/plugins/init.lua
@@ -123,7 +123,7 @@ return {
 			"williamboman/mason.nvim",
 			"williamboman/mason-lspconfig.nvim",
 			-- INFO: null-ls will be archive on Aug 11, 2023, https://github.com/jose-elias-alvarez/null-ls.nvim/issues/1621
-			-- NOTE: alternative: https://github.com/nvimdev/guard.nvim & https://github.com/mfussenegger/nvim-lint
+			-- NOTE: alternative: https://github.com/stevearc/conform.nvim & https://github.com/mfussenegger/nvim-lint
 			"jose-elias-alvarez/null-ls.nvim", -- for formatters and linters
 			"folke/neodev.nvim", -- full signature help, docs and completion for the nvim lua API
 			"b0o/schemastore.nvim", -- providing access to the SchemaStore catalog.
