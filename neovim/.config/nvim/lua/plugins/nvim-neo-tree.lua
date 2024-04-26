4a4d8aa1b2f8d89384676a4d50aed6742055ba39
barryblando <barryblando@gmail.com>
2024-04-26 13:34:45 +0800

    update harpoon component

--- neovim/.config/nvim/lua/plugins/nvim-neo-tree.lua
+++ neovim/.config/nvim/lua/plugins/nvim-neo-tree.lua
@@ -156,27 +156,35 @@ return {
 					never_show = { ".git" },
 				},
 				components = {
-					harpoon_index = function(config, node, state)
-						local Marked = require("harpoon.mark")
+					harpoon_index = function(config, node, _)
+						local harpoon_list = require("harpoon"):list()
 						local path = node:get_id()
-						local succuss, index = pcall(Marked.get_index_of, path)
-						if succuss and index and index > 0 then
-							return {
-								text = string.format("=>%d ", index), -- <-- Add your favorite harpoon like arrow here
-								highlight = config.highlight or "NeoTreeDirectoryIcon",
-							}
-						else
-							return {}
+						local harpoon_key = vim.uv.cwd()
+
+						for i, item in ipairs(harpoon_list.items) do
+							local value = item.value
+							if string.sub(item.value, 1, 1) ~= "/" then
+								value = harpoon_key .. "/" .. item.value
+							end
+
+							if value == path then
+								vim.print(path)
+								return {
+									text = string.format(" => %d", i), -- <-- Add your favorite harpoon like arrow here
+									highlight = config.highlight or "NeoTreeDirectoryIcon",
+								}
+							end
 						end
+						return {}
 					end,
 				},
 				renderers = {
 					file = {
 						{ "icon" },
 						{ "name", use_git_status_colors = true },
-						{ "harpoon_index" }, --> This is what actually adds the component in where you want it
 						{ "diagnostics" },
 						{ "git_status", highlight = "NeoTreeDimText" },
+						{ "harpoon_index" }, --> This is what actually adds the component in where you want it
 					},
 				},
 			},
