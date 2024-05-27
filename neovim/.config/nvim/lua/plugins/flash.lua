eb85dccb6cfbf6644d371c8a984fb139906fc49f
barryblando <barryblando@gmail.com>
2024-05-27 14:25:25 +0800

    remove S R <c-s> keys

--- neovim/.config/nvim/lua/plugins/flash.lua
+++ neovim/.config/nvim/lua/plugins/flash.lua
@@ -1,12 +1,29 @@
 return {
 	"folke/flash.nvim",
 	event = "VeryLazy",
-  -- stylua: ignore
-  keys = {
-    { "s", mode = { "n", "o", "x" }, function() require("flash").jump() end, desc = "Flash" },
-    { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
-  },
+	keys = {
+		{
+			"s",
+			mode = { "n", "o", "x" },
+			function()
+				require("flash").jump()
+			end,
+			desc = "Flash",
+		},
+		{
+			"r",
+			mode = "o",
+			function()
+				require("flash").treesitter_search()
+			end,
+			desc = "Treesitter Search",
+		},
+	},
+	opts = {
+		jump = { nohlsearch = true },
+		modes = {
+			-- Enable flash when searching with ? or /
+			search = { enabled = true },
+		},
+	},
 }
