d6b07f563cb263fdaf9a4c5fbb848562ff65b2a0
barryblando <barryblando@gmail.com>
2024-08-24 12:47:23 +0800

    update some autocmds

--- neovim/.config/nvim/lua/core/autocommands.lua
+++ neovim/.config/nvim/lua/core/autocommands.lua
@@ -54,11 +54,33 @@ autocmd({ "FileType" }, {
 	end,
 })
 
--- Highlight on yank
-autocmd("TextYankPost", {
-	group = augroup("highlight_yank"),
+-- NOTE: workaround for the meantime, gitsigns and statuscol not syncing together
+autocmd({ "BufReadPost", "BufRead" }, {
+	group = augroup("gitsigns_refresh"),
 	callback = function()
-		vim.highlight.on_yank()
+		require("gitsigns").refresh()
+	end,
+})
+
+-- Check if we need to reload the file when it changed
+autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
+	group = augroup("checktime"),
+	callback = function()
+		if vim.o.buftype ~= "nofile" then
+			vim.cmd("checktime")
+		end
+	end,
+})
+
+-- Highlight on yank
+autocmd({ "TextYankPost" }, {
+	group = augroup("yank"),
+	callback = function()
+		if vim.fn.has("wsl") == 1 then
+			vim.fn.system("clip.exe", vim.fn.getreg('"'))
+		else
+			vim.highlight.on_yank()
+		end
 	end,
 })
 
@@ -103,11 +125,10 @@ autocmd({ "CursorHold" }, {
 	end,
 })
 
-local group = vim.api.nvim_create_augroup("__env", { clear = true })
-
+-- Disable diagnostic in env file
 autocmd("BufEnter", {
 	pattern = "*.env*",
-	group = group,
+	group = augroup("_env"),
 	callback = function(args)
 		vim.diagnostic.disable(args.buf)
 	end,
@@ -125,20 +146,28 @@ autocmd({ "BufWritePre" }, {
 	end,
 })
 
--- resize splits if window got resized
+-- Resize splits if window got resized
 autocmd({ "VimResized" }, {
 	group = augroup("resize_splits"),
 	callback = function()
+		local current_tab = vim.fn.tabpagenr()
 		vim.cmd("tabdo wincmd =")
+		vim.cmd("tabnext " .. current_tab)
 	end,
 })
 
--- go to last loc when opening a buffer
+-- Go to last loc when opening a buffer
 autocmd("BufReadPost", {
 	group = augroup("last_loc"),
-	callback = function()
-		local mark = vim.api.nvim_buf_get_mark(0, '"')
-		local lcount = vim.api.nvim_buf_line_count(0)
+	callback = function(event)
+		local exclude = { "gitcommit" }
+		local buf = event.buf
+		if vim.tbl_contains(exclude, vim.bo[buf].filetype) or vim.b[buf].lazyvim_last_loc then
+			return
+		end
+		vim.b[buf].lazyvim_last_loc = true
+		local mark = vim.api.nvim_buf_get_mark(buf, '"')
+		local lcount = vim.api.nvim_buf_line_count(buf)
 		if mark[1] > 0 and mark[1] <= lcount then
 			pcall(vim.api.nvim_win_set_cursor, 0, mark)
 		end
