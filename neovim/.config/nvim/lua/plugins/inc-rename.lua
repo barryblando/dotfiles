f2082e7a463bbc6640ec13a2e041e662f79d78e1
barryblando <barryblando@gmail.com>
2024-08-29 22:10:06 +0800

    nothing new

--- neovim/.config/nvim/lua/plugins/inc-rename.lua
+++ neovim/.config/nvim/lua/plugins/inc-rename.lua
@@ -2,7 +2,7 @@ return {
 	"smjonas/inc-rename.nvim",
 	config = function()
 		require("inc_rename").setup()
-    
+
 		vim.keymap.set("n", "<F2>", function()
 			return ":IncRename " .. vim.fn.expand("<cword>")
 		end, { expr = true })
