ebe295f98cc5645d985e1a3f604bb00935d9fcdb
barryblando <barryblando@gmail.com>
2025-02-26 18:40:20 +0800

    format

--- neovim/.config/nvim/lua/lsp/handlers.lua
+++ neovim/.config/nvim/lua/lsp/handlers.lua
@@ -169,14 +169,14 @@ end
 local function lsp_highlight_document(client)
 	if client.server_capabilities.documentHighlightProvider then
 		vim.cmd([[
-     hi! link LspReferenceRead Visual
-     hi! link LspReferenceText Visual
-     hi! link LspReferenceWrite Visual
-      augroup lsp_document_highlight
-        autocmd! * <buffer>
-        autocmd! CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-        autocmd! CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-      augroup END
+       hi! link LspReferenceRead Visual
+       hi! link LspReferenceText Visual
+       hi! link LspReferenceWrite Visual
+       augroup lsp_document_highlight
+       autocmd! * <buffer>
+       autocmd! CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+       autocmd! CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+       augroup END
     ]])
 	end
 end
