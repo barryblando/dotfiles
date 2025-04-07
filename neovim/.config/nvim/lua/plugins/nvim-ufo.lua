76c5a4675001ddb6ffed61da117bd341743e0ff8
barryblando <barryblando@gmail.com>
2025-04-07 15:03:24 +0800

    transfer back K keymap to handlers

--- neovim/.config/nvim/lua/plugins/nvim-ufo.lua
+++ neovim/.config/nvim/lua/plugins/nvim-ufo.lua
@@ -149,13 +149,6 @@ return {
 			vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
 			vim.keymap.set("n", "zr", require("ufo").openFoldsExceptKinds)
 			vim.keymap.set("n", "zm", require("ufo").closeFoldsWith) -- closeAllFolds == closeFoldsWith(0)
-			vim.keymap.set("n", "K", function()
-				local winid = require("ufo").peekFoldedLinesUnderCursor()
-				if not winid then
-					-- choose one of coc.nvim and nvim lsp
-					vim.lsp.buf.hover()
-				end
-			end)
 		end,
 	},
 }
