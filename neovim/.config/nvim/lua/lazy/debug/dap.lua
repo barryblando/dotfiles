d40eb396fade3d1a8c0373aabe5c50225001159a
barryblando <barryblando@gmail.com>
2025-04-12 10:35:28 +0800

    put dap in debug

---
+++ neovim/.config/nvim/lua/lazy/debug/dap.lua
@@ -0,0 +1,27 @@
+-- lua/lazy/debug/dap.lua
+
+local icons = require("core.icons")
+
+for name, sign in pairs(icons.dap) do
+	local sign_value = type(sign) == "table" and sign or { sign }
+	vim.fn.sign_define("Dap" .. name, {
+		text = sign_value[1] .. " ",
+		texthl = sign_value[2] or "DiagnosticInfo",
+		linehl = sign_value[3],
+		numhl = sign_value[3],
+	})
+end
+
+return {
+	{
+		"mfussenegger/nvim-dap",
+		dependencies = {
+			"nvim-neotest/nvim-nio",
+			{ "rcarriga/nvim-dap-ui", opts = require("plugins.dap.ui").opts },
+			{ "theHamsta/nvim-dap-virtual-text", opts = require("plugins.dap.virtual-text").opts },
+			{ "jbyuki/one-small-step-for-vimkind", init = require("plugins.dap.lua").init },
+		},
+		keys = require("core.keymaps").setup_dap_keymaps(),
+		config = require("plugins.dap").config,
+	},
+}
