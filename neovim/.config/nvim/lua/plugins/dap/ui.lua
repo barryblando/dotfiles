dd3c4b97bd61332530347a151eedbcfce21b8e5f
barryblando <barryblando@gmail.com>
2025-04-20 09:24:22 +0800

    default the layout

--- neovim/.config/nvim/lua/plugins/dap/ui.lua
+++ neovim/.config/nvim/lua/plugins/dap/ui.lua
@@ -8,16 +8,35 @@ return {
 			expanded = icons.arrows.down,
 		},
 		floating = { border = icons.ui.Border_Single_Line },
-		layouts = {
-			{
-				elements = {
-					{ id = "stacks", size = 0.30 },
-					{ id = "breakpoints", size = 0.20 },
-					{ id = "scopes", size = 0.50 },
-				},
-				position = "left",
-				size = 40,
-			},
-		},
+		-- layouts = {
+		-- {
+		-- 	elements = {
+		-- 		{ id = "stacks", size = 0.30 },
+		-- 		{ id = "breakpoints", size = 0.20 },
+		-- 		{ id = "scopes", size = 0.50 },
+		-- 	},
+		-- 	position = "left",
+		-- 	size = 40,
+		-- },
+		-- INFO: Alternative layout
+		-- {
+		-- 	elements = {
+		-- 		{ id = "stacks", size = 0.40 },
+		-- 		{ id = "breakpoints", size = 0.20 },
+		-- 		{ id = "scopes", size = 0.40 },
+		-- 		{ id = "watches", size = 0.20 },
+		-- 	},
+		-- 	position = "left",
+		-- 	size = 50,
+		-- },
+		-- {
+		-- 	elements = {
+		-- 		{ id = "console", size = 0.20 },
+		-- 		{ id = "repl", size = 0.50 },
+		-- 	},
+		-- 	position = "bottom",
+		-- 	size = 10,
+		-- },
+		-- },
 	},
 }
