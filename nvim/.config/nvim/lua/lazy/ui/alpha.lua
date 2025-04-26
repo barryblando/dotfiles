97977d97e2d93bfd84b0d8d61bee283977998637
barryblando <barryblando@gmail.com>
2025-04-26 21:08:46 +0800

    transfer key to which-key

--- nvim/.config/nvim/lua/lazy/ui/alpha.lua
+++ nvim/.config/nvim/lua/lazy/ui/alpha.lua
@@ -2,9 +2,6 @@ return {
 	"goolord/alpha-nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	lazy = false,
-	keys = {
-		{ "<leader>a", "<cmd>Alpha<cr>", desc = "Alpha Screen" },
-	},
 	event = "VimEnter",
 	opts = require("plugins.alpha").opts,
 	config = require("plugins.alpha").config,
