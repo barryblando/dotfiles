6b589aa31b5735d48f98c06d41e5b542c15fea24
barryblando <barryblando@gmail.com>
2025-04-27 22:22:06 +0800

    update rg and git

--- nvim/.config/nvim/lua/plugins/fzf-lua.lua
+++ nvim/.config/nvim/lua/plugins/fzf-lua.lua
@@ -1,13 +1,35 @@
 local M = {}
 
 M.config = function()
+	-- Default fallback settings
+	local default_border = "border-bottom"
+	local default_pager = "delta --line-numbers"
+
+	-- Special overrides per section (only if you want to override)
+	local overrides = {
+		commits = { pager = "delta --line-numbers", border = "border-bottom" },
+		bcommits = { pager = "delta --line-numbers", border = "border-bottom" },
+		branches = { pager = "less", border = "border-top" },
+		stash = { pager = "less", border = "border-top" },
+		status = { pager = "bat --plain", border = "border-bottom" },
+		tag = { pager = "bat --plain", border = "border-bottom" },
+		blame = { pager = "bat --plain", border = "border-bottom" },
+	}
+
+	-- All git sections
+	local sections = { "commits", "bcommits", "branches", "stash", "status", "tag", "blame" }
+
+	-- Build dynamically
 	local git_settings = {}
-	for _, section in ipairs({ "commits", "bcommits", "branches", "stash", "status", "tag", "blame" }) do
+
+	for _, section in ipairs(sections) do
+		local o = overrides[section] or {}
 		git_settings[section] = {
-			winopts = { preview = { border = "border-bottom" } },
-			preview_pager = "delta --line-numbers",
+			winopts = { preview = { border = o.border or default_border } },
+			preview_pager = o.pager or default_pager,
 		}
 	end
+
 	require("fzf-lua").setup({
 		file_icon_padding = " ",
 		winopts = {
@@ -27,7 +49,23 @@ M.config = function()
 			file_icons = true,
 		},
 		grep = {
-			rg_opts = "--hidden --column --line-number --no-heading --color=always --smart-case -g '!node_modules/*'",
+			-- rg_opts = "--hidden --column --line-number --no-heading --color=always --smart-case -g '!node_modules/*'",
+			rg_opts = table.concat({
+				"--hidden", -- include hidden files
+				"--no-ignore-vcs", -- search in .gitignored files too
+				"--glob=!node_modules/*", -- ignore node_modules
+				"--glob=!vendor/*", -- ignore vendor/ if you do Go/PHP
+				"--glob=!.git/*", -- ignore .git folder (speed up)
+				"--column", -- show column number
+				"--line-number", -- show line number
+				"--no-heading", -- no file heading, easier fzf parsing
+				"--color=always", -- force color output (important)
+				"--smart-case", -- smart case (case insensitive if all lowercase)
+				"--trim", -- trim whitespace
+				"--max-columns=4096", -- avoid slow parsing with huge files
+				"--follow", -- follow symlinks
+				"--ignore-case", -- optionally force ignore-case
+			}, " "),
 		},
 		git = git_settings,
 		lsp = { code_actions = { previewer = "codeaction_native" } },
