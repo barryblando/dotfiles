95d935fad6b4e1fec9ac2208375f13065e4a8d5e
barryblando <barryblando@gmail.com>
2025-04-27 13:33:42 +0800

    disable input as of now

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -33,43 +33,52 @@ end
 M.config = function()
 	local icons = require("core.icons")
 	require("snacks").setup({
-		input = {
-			enabled = true,
-			backdrop = false,
-			position = "float",
-			border = icons.ui.Border_Chars_Single_Line,
-			title_pos = "center",
-			height = 1,
-			width = 60,
-			relative = "editor",
-			noautocmd = true,
-			row = 2,
-			-- relative = "cursor",
-			-- row = -3,
-			-- col = 0,
-			wo = {
-				winhighlight = "NormalFloat:SnacksInputNormal,FloatBorder:SnacksInputBorder,FloatTitle:SnacksInputTitle",
-				cursorline = false,
-			},
-			bo = {
-				filetype = "snacks_input",
-				buftype = "prompt",
-			},
-			--- buffer local variables
-			b = {
-				completion = false, -- disable blink completions in input
-			},
-			keys = {
-				n_esc = { "<esc>", { "cmp_close", "cancel" }, mode = "n", expr = true },
-				i_esc = { "<esc>", { "cmp_close", "stopinsert" }, mode = "i", expr = true },
-				i_cr = { "<cr>", { "cmp_accept", "confirm" }, mode = { "i", "n" }, expr = true },
-				i_tab = { "<tab>", { "cmp_select_next", "cmp" }, mode = "i", expr = true },
-				i_ctrl_w = { "<c-w>", "<c-s-w>", mode = "i", expr = true },
-				i_up = { "<up>", { "hist_up" }, mode = { "i", "n" } },
-				i_down = { "<down>", { "hist_down" }, mode = { "i", "n" } },
-				q = "cancel",
-			},
-		},
+		-- input = {
+		-- 	icon = " ",
+		-- 	icon_hl = "SnacksInputIcon",
+		-- 	icon_pos = "left",
+		-- 	prompt_pos = "title",
+		-- 	win = { style = "input" },
+		-- 	expand = true,
+		-- },
+		-- styles = {
+		-- 	input = {
+		-- 		border = icons.ui.Border_Solid_Line,
+		-- 		backdrop = false,
+		-- 		position = "float",
+		-- 		title_pos = "center",
+		-- 		height = 1,
+		-- 		width = 60,
+		-- 		-- relative = "editor",
+		-- 		noautocmd = true,
+		-- 		row = 2,
+		-- 		-- relative = "cursor",
+		-- 		-- row = -3,
+		-- 		-- col = 0,
+		-- 		wo = {
+		-- 			winhighlight = "NormalFloat:SnacksInputNormal,FloatBorder:SnacksInputBorder,FloatTitle:SnacksInputTitle",
+		-- 			cursorline = false,
+		-- 		},
+		-- 		bo = {
+		-- 			filetype = "snacks_input",
+		-- 			buftype = "prompt",
+		-- 		},
+		-- 		--- buffer local variables
+		-- 		b = {
+		-- 			completion = false, -- disable blink completions in input
+		-- 		},
+		-- 	},
+		-- },
+		-- keys = {
+		-- 	n_esc = { "<esc>", { "cmp_close", "cancel" }, mode = "n", expr = true },
+		-- 	i_esc = { "<esc>", { "cmp_close", "stopinsert" }, mode = "i", expr = true },
+		-- 	i_cr = { "<cr>", { "cmp_accept", "confirm" }, mode = { "i", "n" }, expr = true },
+		-- 	i_tab = { "<tab>", { "cmp_select_next", "cmp" }, mode = "i", expr = true },
+		-- 	i_ctrl_w = { "<c-w>", "<c-s-w>", mode = "i", expr = true },
+		-- 	i_up = { "<up>", { "hist_up" }, mode = { "i", "n" } },
+		-- 	i_down = { "<down>", { "hist_down" }, mode = { "i", "n" } },
+		-- 	q = "cancel",
+		-- },
 	})
 end
 
