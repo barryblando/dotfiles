769b7a9afd697a983c12c1620995cee14a10b9da
barryblando <barryblando@gmail.com>
2025-04-27 15:43:35 +0800

    remove preview border in git

--- nvim/.config/nvim/lua/plugins/fzf-lua.lua
+++ nvim/.config/nvim/lua/plugins/fzf-lua.lua
@@ -1,6 +1,13 @@
 local M = {}
 
 M.config = function()
+	local git_settings = {}
+	for _, section in ipairs({ "commits", "bcommits", "branches", "stash", "status", "tag", "blame" }) do
+		git_settings[section] = {
+			winopts = { preview = { border = "border-bottom" } },
+			preview_pager = "delta --line-numbers",
+		}
+	end
 	require("fzf-lua").setup({
 		file_icon_padding = " ",
 		winopts = {
@@ -22,11 +29,7 @@ M.config = function()
 		grep = {
 			rg_opts = "--hidden --column --line-number --no-heading --color=always --smart-case -g '!node_modules/*'",
 		},
-		git = {
-			commits = {
-				preview_pager = "delta --line-numbers",
-			},
-		},
+		git = git_settings,
 		lsp = { code_actions = { previewer = "codeaction_native" } },
 	})
 end
