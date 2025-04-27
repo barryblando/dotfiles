9a5c1b650d211c8280d7f366e177f8dc81066414
barryblando <barryblando@gmail.com>
2025-04-27 13:29:40 +0800

    set lazy to false and add keymaps

--- nvim/.config/nvim/lua/lazy/tools/fzf-lua.lua
+++ nvim/.config/nvim/lua/lazy/tools/fzf-lua.lua
@@ -1,8 +1,10 @@
 return {
 	"ibhagwan/fzf-lua",
+	lazy = false,
 	-- optional for icon support
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	-- or if using mini.icons/mini.nvim
 	-- dependencies = { "echasnovski/mini.icons" },
+	keys = require("core.keymaps").setup_fzf_lua_keymaps,
 	config = require("plugins.fzf-lua").config,
 }
