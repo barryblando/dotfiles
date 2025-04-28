05e9a2014b8ce6a27312a38d49a8c0faadaf74cb
barryblando <barryblando@gmail.com>
2025-04-28 22:55:08 +0800

    clean up

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -2,14 +2,7 @@ local M = {}
 
 M.config = function()
 	local harpoon = require("harpoon")
-	local functions = require("core.utils")
 	harpoon:setup({})
-
-	vim.keymap.set("n", "<C-e>", function()
-		functions.harpoon_ui(harpoon:list())
-	end, { desc = "Open Harpoon Window" })
-
-	vim.cmd([[ command! HarpoonUI execute 'lua require("core.utils").harpoon_ui(require("harpoon"):list())' ]])
 end
 
 return M
