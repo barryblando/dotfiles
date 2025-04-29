9f4f7c6e9d168715d0b60bc719ffb960ceaaa70c
barryblando <barryblando@gmail.com>
2025-04-29 15:09:01 +0800

    update

--- nvim/.config/nvim/after/ftplugin/rust.lua
+++ nvim/.config/nvim/after/ftplugin/rust.lua
@@ -3,24 +3,21 @@ if not status_ok then
 	return
 end
 
-wk.add({
-	{ "<leader>L", group = "Rust", nowait = true, remap = false },
-	{ "<leader>LH", "<cmd>RustDisableInlayHints<Cr>", desc = "Disable Hints", nowait = true, remap = false },
-	{
-		"<leader>LR",
-		"<cmd>lua require('rust-tools/workspace_refresh')._reload_workspace_from_cargo_toml()<Cr>",
-		desc = "Reload Workspace",
-		nowait = true,
-		remap = false,
-	},
-	{ "<leader>Lc", "<cmd>RustOpenCargo<Cr>", desc = "Open Cargo", nowait = true, remap = false },
-	{ "<leader>Ld", "<cmd>RustDebuggables<Cr>", desc = "Debuggables", nowait = true, remap = false },
-	{ "<leader>Lm", "<cmd>RustExpandMacro<Cr>", desc = "Expand Macro", nowait = true, remap = false },
-	{ "<leader>Lo", "<cmd>RustOpenExternalDocs<Cr>", desc = "Open External Docs", nowait = true, remap = false },
-	{ "<leader>Lp", "<cmd>RustParentModule<Cr>", desc = "Parent Module", nowait = true, remap = false },
-	{ "<leader>Lr", "<cmd>RustRunnables<Cr>", desc = "Runnables", nowait = true, remap = false },
-	{ "<leader>Lt", "<cmd>lua _CARGO_TEST()<cr>", desc = "Cargo Test", nowait = true, remap = false },
-	{ "<leader>Lv", "<cmd>RustViewCrateGraph<Cr>", desc = "View Crate Graph", nowait = true, remap = false },
-})
+vim.schedule(function()
+	local common_opts = { buffer = true, nowait = true, remap = false }
 
-vim.api.nvim_set_keymap("n", "<m-d>", "<cmd>RustOpenExternalDocs<Cr>", { noremap = true, silent = true })
+	-- Define the keymaps and their properties
+	local keymaps = {
+		{ "<leader>l", group = "LSP", icon = { icon = " " } },
+		{ "<leader>lr", "<cmd>RustAnalyzer restart<cr>", desc = "Restart Server" },
+	}
+
+	-- Iterate through the keymaps and add common options
+	local combined_keymaps = {}
+	for _, map in ipairs(keymaps) do
+		local combined_map = vim.tbl_extend("force", map, common_opts)
+		table.insert(combined_keymaps, combined_map)
+	end
+
+	wk.add(combined_keymaps)
+end)
