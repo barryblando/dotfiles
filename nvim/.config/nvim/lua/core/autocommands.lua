0e7f0b8f49b97d907c2863c045d92925197ac7f3
barryblando <barryblando@gmail.com>
2025-04-29 11:32:13 +0800

    cleanup

--- nvim/.config/nvim/lua/core/autocommands.lua
+++ nvim/.config/nvim/lua/core/autocommands.lua
@@ -220,33 +220,6 @@ vim.api.nvim_set_hl(0, "TransparentSearch", { fg = search.foreground })
 local help = vim.api.nvim_get_hl_by_name("IncSearch", true)
 vim.api.nvim_set_hl(0, "TransparentHelp", { fg = help.foreground })
 
-local cmdGroup = "DevIconLua"
-local noice_cmd_types = {
-	CmdLine = cmdGroup,
-	Input = cmdGroup,
-	Lua = cmdGroup,
-	Filter = cmdGroup,
-	Rename = cmdGroup,
-	Substitute = "Define",
-	-- Help       = "TransparentHelp",
-	-- Search     = "TransparentSearch"
-	Help = "Constant",
-	Search = "Constant",
-}
-
--- local noice_hl = vim.api.nvim_create_augroup("NoiceHighlights", {})
--- vim.api.nvim_clear_autocmds({ group = noice_hl })
--- autocmd("BufEnter", {
--- 	group = noice_hl,
--- 	callback = function()
--- 		for type, hl in pairs(noice_cmd_types) do
--- 			vim.api.nvim_set_hl(0, "NoiceCmdlinePopupBorder" .. type, { link = hl })
--- 			vim.api.nvim_set_hl(0, "NoiceCmdlineIcon" .. type, { link = hl })
--- 		end
--- 		vim.api.nvim_set_hl(0, "NoiceConfirmBorder", { link = cmdGroup })
--- 	end,
--- })
-
 -- show cursor line only in active window
 autocmd({ "InsertLeave", "WinEnter" }, {
 	callback = function()
