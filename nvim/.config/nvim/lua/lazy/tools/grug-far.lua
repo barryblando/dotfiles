7229a114a61d9310b393db4ac374fe1349d1971b
barryblando <barryblando@gmail.com>
2025-05-01 22:52:13 +0800

    refactor keys

--- nvim/.config/nvim/lua/lazy/tools/grug-far.lua
+++ nvim/.config/nvim/lua/lazy/tools/grug-far.lua
@@ -1,8 +1,6 @@
 return {
 	"MagicDuck/grug-far.nvim",
 	cmd = "GrugFar",
-	init = function()
-		vim.keymap.set("n", "<leader>R", "<cmd>GrugFar<cr>", { desc = "GrugFar | Find And Replace", silent = true })
-	end,
-	opts = {},
+	keys = require("plugins.grug-far").keys,
+	config = require("plugins.grug-far").config,
 }
