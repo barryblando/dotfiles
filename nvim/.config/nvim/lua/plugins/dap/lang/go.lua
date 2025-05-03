6645f7ba484a2e6062f26bc68a3aa8f5fb688342
barryblando <barryblando@gmail.com>
2025-05-03 12:25:31 +0800

    folder language specifc dap config

---
+++ nvim/.config/nvim/lua/plugins/dap/lang/go.lua
@@ -0,0 +1,28 @@
+local M = {}
+
+M.opts = {
+	dap_configurations = {
+		{
+			type = "go",
+			name = "Attach remote",
+			mode = "remote",
+			request = "attach",
+		},
+	},
+	-- delve configurations
+	delve = {
+		path = "dlv",
+		initialize_timeout_sec = 20,
+		port = "${port}",
+		args = {},
+		build_flags = {},
+		detached = vim.fn.has("win32") == 0,
+		cwd = nil,
+	},
+	tests = {
+		-- enables verbosity when running the test.
+		verbose = false,
+	},
+}
+
+return M
