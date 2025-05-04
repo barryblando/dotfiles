82b93e5e4b230df0f5d234b89f814eaa8bd78c1e
barryblando <barryblando@gmail.com>
2025-05-04 14:55:49 +0800

    clean setup

--- nvim/.config/nvim/lua/lsp/mason.lua
+++ nvim/.config/nvim/lua/lsp/mason.lua
@@ -8,10 +8,43 @@ if not mlsp_status_ok then
 	return
 end
 
+local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
+if not lspconfig_status_ok then
+	return
+end
+
+local mdap_status_ok, mason_dap = pcall(require, "mason-nvim-dap")
+if not mdap_status_ok then
+	return
+end
+
 local icons = require("core.icons")
 
+-- https://mason-registry.dev/registry/list
+local mason_packages = {
+	"stylua",
+	"black",
+	"isort",
+	"prettier",
+	"prettierd",
+}
+
+mason.setup({
+	ensure_installed = mason_packages,
+	ui = {
+		border = icons.ui.Border_Single_Line,
+		icons = {
+			package_installed = "✓",
+			package_pending = "➜",
+			package_uninstalled = "✗",
+		},
+	},
+	log_level = vim.log.levels.INFO,
+	max_concurrent_installers = 4,
+})
+
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-local servers = {
+local lsp_servers = {
 	"bashls",
 	"denols",
 	"dockerls",
@@ -35,48 +68,26 @@ local servers = {
 	"eslint",
 }
 
--- https://mason-registry.dev/registry/list
-local packages = {
-	-- debuggers
-	"codelldb",
-	"delve",
-
-	-- formatters
-	"stylua",
-	"black",
-	"isort",
-	"prettier",
-	"prettierd",
-}
-
-local settings = {
-	ensure_installed = packages,
-	ui = {
-		border = icons.ui.Border_Single_Line,
-		icons = {
-			package_installed = "✓",
-			package_pending = "➜",
-			package_uninstalled = "✗",
-		},
-	},
-	log_level = vim.log.levels.INFO,
-	max_concurrent_installers = 4,
-}
-
-mason.setup(settings)
 mason_lspconfig.setup({
-	ensure_installed = servers,
+	ensure_installed = lsp_servers,
 	automatic_installation = true,
 })
 
-local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
-if not lspconfig_status_ok then
-	return
-end
+-- https://github.com/jay-babu/mason-nvim-dap.nvim/blob/main/lua/mason-nvim-dap/mappings/source.lua
+local dap_packages = {
+	"codelldb",
+	"node2",
+	"delve",
+}
+
+mason_dap.setup({
+	ensure_installed = dap_packages,
+	automatic_installation = true,
+})
 
 local opts = {}
 
-for _, server in pairs(servers) do
+for _, server in pairs(lsp_servers) do
 	opts = {
 		on_attach = require("lsp.handlers").on_attach,
 		capabilities = require("lsp.handlers").capabilities,
