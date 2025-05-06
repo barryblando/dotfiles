d46b7f5e54c049193b347a98c2084ed9860a6a32
barryblando <barryblando@gmail.com>
2025-05-06 19:59:54 +0800

    lsp legacy setup

---
+++ nvim/.config/nvim/lua/lsp/legacy_setup.lua
@@ -0,0 +1,40 @@
+-- local mlsp_status_ok, mason_lspconfig = pcall(require, "mason-lspconfig")
+-- if not mlsp_status_ok then
+-- 	return
+-- end
+
+-- local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
+-- if not lspconfig_status_ok then
+-- 	return
+-- end
+
+-- local mdap_status_ok, mason_dap = pcall(require, "mason-nvim-dap")
+-- if not mdap_status_ok then
+-- 	return
+-- end
+
+-- Ensure packages are installed and up to date
+-- registry.refresh(function()
+-- 	for _, name in pairs(ensure_installed) do
+-- 		local package = registry.get_package(name)
+-- 		if not registry.is_installed(name) then
+-- 			package:install()
+-- 		else
+-- 			package:check_new_version(function(success, result_or_err)
+-- 				if success then
+-- 					package:install({ version = result_or_err.latest_version })
+-- 				end
+-- 			end)
+-- 		end
+-- 	end
+-- end)
+
+-- mason_lspconfig.setup({
+-- 	ensure_installed = lsp_servers,
+-- 	automatic_installation = true,
+-- })
+
+-- mason_dap.setup({
+-- 	ensure_installed = dap_extensions,
+-- 	automatic_installation = true,
+-- })
