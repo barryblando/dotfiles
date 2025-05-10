d1efbcac632f9da6d771f1981f45239119b41b9a
barryblando <barryblando@gmail.com>
2025-05-10 22:12:07 +0800

    add autocomplete for learn-rust

---
+++ fish/.config/fish/completions/learn-rust.fish
@@ -0,0 +1,6 @@
+complete -c learn-rust -l init -d "Initialize new Rust workspace"
+complete -c learn-rust -l name -r -d "Workspace name"
+complete -c learn-rust -l lesson -r -d "Add new lesson (auto-numbered)"
+complete -c learn-rust -l remove -r -d "Remove lesson by number"
+complete -c learn-rust -l reset -d "Reset and remove all lessons"
+
