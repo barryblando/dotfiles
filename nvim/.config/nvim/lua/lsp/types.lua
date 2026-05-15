7f95f08732124c8ccfe979f3f863ecc1faf4a825
barryblando <barryblando@gmail.com>
2026-05-15 18:44:51 +0800

    update deprecated function

--- nvim/.config/nvim/lua/lsp/types.lua
+++ nvim/.config/nvim/lua/lsp/types.lua
@@ -9,4 +9,4 @@ vim = require("vim.inspect")
 -- let sumneko know where the sources are for the global vim runtime
 vim.lsp = require("vim.lsp")
 vim.treesitter = require("vim.treesitter")
-vim.highlight = require("vim.highlight")
+vim.hl = require("vim.hl")
